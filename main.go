4e7baf2c51616e034e0ed1c0367484fcd7ff8dea
Franklin Isalguez <franklin@neosmail.com>
2026-05-27 10:02:37 -0400

    1-5 Add exit and help commands

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		text := scanner.Text()
-		inputs := cleanInput(text)
-		command := inputs[0]
-
-		fmt.Printf("Your command was: %s\n", command)
-
-	}
+	startRepl()
 }
